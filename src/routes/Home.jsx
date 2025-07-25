826c6a138d13ccb9b6b1732c12728fbbff993a5e
Samuel Nunes <samuelsalomao883@gmail.com>
2025-07-25 08:24:31 -0300

    Get dos posts

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import axios from "axios";
+
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import './Home.css';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+      const data = response.data;
+      console.log("data =>", data);
+
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, []);
+
   return (
-    <>
-      Home
-    </>
+    <div>
+      <h1>Ultimos posts</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
   )
 };
